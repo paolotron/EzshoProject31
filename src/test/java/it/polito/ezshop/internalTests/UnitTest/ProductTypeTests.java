36285852e7bef04494e2811666df620645bc9ee4
s284365 <s284365@studenti.polito.it>
2021-05-17 19:40:31 +0200

    added BarCodeAlgo test

--- src/test/java/it/polito/ezshop/internalTests/UnitTest/ProductTypeTests.java
+++ src/test/java/it/polito/ezshop/internalTests/UnitTest/ProductTypeTests.java
@@ -7,11 +7,23 @@ import org.junit.jupiter.api.Test;
 public class ProductTypeTests {
 
     @Test
-    void testBarcodeAlgorithm(){
+    void testCorrectBarcodeAlgorithm(){
         Assertions.assertTrue(ProductTypeModel.checkBarCodeWithAlgorithm("6291041500213"));
+        Assertions.assertTrue(ProductTypeModel.checkBarCodeWithAlgorithm("62910415002134"));
+        Assertions.assertTrue(ProductTypeModel.checkBarCodeWithAlgorithm("7482156234585"));
+        Assertions.assertTrue(ProductTypeModel.checkBarCodeWithAlgorithm("47845126544844"));
+        Assertions.assertTrue(ProductTypeModel.checkBarCodeWithAlgorithm("989661725630"));
+    }
+
+    @Test
+    void testWrongBarcodeAlgorithm(){
         Assertions.assertFalse(ProductTypeModel.checkBarCodeWithAlgorithm("6291041500211"));
         Assertions.assertFalse(ProductTypeModel.checkBarCodeWithAlgorithm("1242"));
         Assertions.assertFalse(ProductTypeModel.checkBarCodeWithAlgorithm(null));
-        Assertions.assertTrue(ProductTypeModel.checkBarCodeWithAlgorithm("62910415002134"));
+        Assertions.assertFalse(ProductTypeModel.checkBarCodeWithAlgorithm("ABCD"));
+        Assertions.assertFalse(ProductTypeModel.checkBarCodeWithAlgorithm("!*){"));
     }
+
+
+
 }
