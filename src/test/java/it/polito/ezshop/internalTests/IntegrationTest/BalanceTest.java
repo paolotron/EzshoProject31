dec52e7ea087f3f0c8933d1c7f7d014e5b52ab5b
MANUEL MESSINA s282588 <s282588@studenti.polito.it>
2021-05-19 16:24:47 +0200

    BalanceModelTest into BalanceTest unit to Integration

--- src/test/java/it/polito/ezshop/internalTests/IntegrationTest/BalanceTest.java
+++ src/test/java/it/polito/ezshop/internalTests/IntegrationTest/BalanceTest.java
@@ -4,11 +4,16 @@ package it.polito.ezshop.internalTests.IntegrationTest;
 import it.polito.ezshop.data.EZShopInterface;
 import it.polito.ezshop.exceptions.*;
 
+import it.polito.ezshop.model.BalanceModel;
+import it.polito.ezshop.model.BalanceOperationModel;
+import it.polito.ezshop.model.SaleModel;
+import it.polito.ezshop.model.SaleTransactionModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 
 public class BalanceTest {
@@ -28,7 +33,7 @@ public class BalanceTest {
     }
 
     @BeforeEach
-    void startEzShop() throws UnauthorizedException, InvalidProductDescriptionException, InvalidPricePerUnitException, InvalidProductCodeException, InvalidPasswordException, InvalidRoleException, InvalidUsernameException {
+    void startEzShop() throws InvalidPasswordException, InvalidRoleException, InvalidUsernameException {
         model = new it.polito.ezshop.data.EZShop();
         model.reset();
         model.createUser(username, password, "Administrator");
@@ -71,11 +76,33 @@ public class BalanceTest {
     } DOES NOT WORK IF YOU DON'T MAKE THE EXECUTION EXACTLY AT MIDNIGHT*/
 
     @Test
-    void unauthorizedShowCreditsAndDebits() throws InvalidPasswordException, InvalidUsernameException, UnauthorizedException {
+    void unauthorizedShowCreditsAndDebits() throws InvalidPasswordException, InvalidUsernameException {
         unauthorized_login();
         LocalDate d1 = LocalDate.now();
         Assertions.assertThrows(UnauthorizedException.class, () -> model.getCreditsAndDebits(d1, d1));
     }
 
+    @Test
+    public void getTransactionByIdTest(){
+        BalanceModel b = new BalanceModel();
+        BalanceOperationModel credit = new BalanceOperationModel("CREDIT", 50., LocalDate.now());
+        BalanceOperationModel debit = new BalanceOperationModel("DEBIT", -50., LocalDate.now());
+        SaleTransactionModel sale = new SaleTransactionModel(new SaleModel());
+
+        b.addBalanceOperation(credit);
+        b.addBalanceOperation(debit);
+        b.addBalanceOperation(sale);
+
+        Optional<BalanceOperationModel> result = b.getTransactionById(credit.getBalanceId());
+        Assertions.assertEquals(credit, result.orElse(null));
+        result = b.getTransactionById(debit.getBalanceId());
+        Assertions.assertEquals(debit, result.orElse(null));
+        result = b.getTransactionById(sale.getBalanceId());
+        Assertions.assertEquals(sale, result.orElse(null));
+        result = b.getTransactionById(100);
+        Assertions.assertNull(result.orElse(null));
+        result = b.getTransactionById(-10);
+        Assertions.assertNull(result.orElse(null));
+    }
 
 }
