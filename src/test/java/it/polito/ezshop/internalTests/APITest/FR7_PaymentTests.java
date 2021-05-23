9533c753dfe25a5a49f7f45da6d1f25c1c559116
MANUEL MESSINA s282588 <s282588@studenti.polito.it>
2021-05-23 16:48:48 +0200

    push FR7 tests

--- src/test/java/it/polito/ezshop/internalTests/APITest/FR7_PaymentTests.java
+++ src/test/java/it/polito/ezshop/internalTests/APITest/FR7_PaymentTests.java
@@ -1,4 +1,135 @@
 package it.polito.ezshop.internalTests.APITest;
 
+import it.polito.ezshop.data.EZShopInterface;
+import it.polito.ezshop.exceptions.*;
+import it.polito.ezshop.model.EzShopModel;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
 public class FR7_PaymentTests {
+    EZShopInterface model;
+    EzShopModel ez;
+    final String admin_username = "Admin";
+    final String admin_psw = "password12345678";
+    final String productCode = "6291041500213";
+
+    @Before
+    public void startEzShop() throws InvalidPasswordException, InvalidRoleException, InvalidUsernameException, UnauthorizedException, InvalidProductDescriptionException, InvalidPricePerUnitException, InvalidProductCodeException {
+        model = new it.polito.ezshop.data.EZShop();
+        ez = new EzShopModel();
+        model.reset();
+        model.createUser(admin_username, admin_psw, "Administrator");
+        model.login(admin_username, admin_psw);
+        model.createProductType("desc", productCode, 5., "note");
+        model.logout();
+    }
+    @After
+    public void closeEzShop(){
+        model.reset();
+    }
+
+    @Test
+    public void invalidTransactionIdTest() throws InvalidPasswordException, InvalidUsernameException {
+        model.login(admin_username, admin_psw);
+
+        assertThrows(InvalidTransactionIdException.class, ()->model.receiveCashPayment(0, 50));
+        assertThrows(InvalidTransactionIdException.class, ()->model.receiveCashPayment(-12, 50));
+        assertThrows(InvalidTransactionIdException.class, ()->model.receiveCashPayment(null, 50));
+
+        assertThrows(InvalidTransactionIdException.class, ()->model.receiveCreditCardPayment(0, "4485370086510891"));
+        assertThrows(InvalidTransactionIdException.class, ()->model.receiveCreditCardPayment(-12, "4485370086510891"));
+        assertThrows(InvalidTransactionIdException.class, ()->model.receiveCreditCardPayment(null, "4485370086510891"));
+
+        assertThrows(InvalidTransactionIdException.class, ()->model.returnCashPayment(0));
+        assertThrows(InvalidTransactionIdException.class, ()->model.returnCashPayment(-12));
+        assertThrows(InvalidTransactionIdException.class, ()->model.returnCashPayment(null));
+
+        assertThrows(InvalidTransactionIdException.class, ()->model.returnCreditCardPayment(0, "4485370086510891"));
+        assertThrows(InvalidTransactionIdException.class, ()->model.returnCreditCardPayment(-12, "4485370086510891"));
+        assertThrows(InvalidTransactionIdException.class, ()->model.returnCreditCardPayment(null, "4485370086510891"));
+    }
+
+    @Test
+    public void unauthorizedTest(){
+        assertThrows(UnauthorizedException.class, ()->model.receiveCashPayment(1, 50));
+        assertThrows(UnauthorizedException.class, ()->model.receiveCreditCardPayment(1, "4485370086510891"));
+        assertThrows(UnauthorizedException.class, ()->model.returnCashPayment(1));
+        assertThrows(UnauthorizedException.class, ()->model.returnCreditCardPayment(1, "4485370086510891"));
+    }
+
+    @Test
+    public void invalidPaymentTest() throws InvalidPasswordException, InvalidUsernameException {
+        model.login(admin_username, admin_psw);
+
+        assertThrows(InvalidPaymentException.class, ()->model.receiveCashPayment(1, -50));
+        assertThrows(InvalidPaymentException.class, ()->model.receiveCashPayment(1, 0));
+    }
+
+    @Test
+    public void invalidCreditCard() throws InvalidPasswordException, InvalidUsernameException {
+        model.login(admin_username, admin_psw);
+
+        assertThrows(InvalidCreditCardException.class, ()->model.receiveCreditCardPayment(1, ""));
+        assertThrows(InvalidCreditCardException.class, ()->model.receiveCreditCardPayment(1, null));
+        assertThrows(InvalidCreditCardException.class, ()->model.receiveCreditCardPayment(1, "111111111111111"));
+
+        assertThrows(InvalidCreditCardException.class, ()->model.returnCreditCardPayment(1, ""));
+        assertThrows(InvalidCreditCardException.class, ()->model.returnCreditCardPayment(1, null));
+        assertThrows(InvalidCreditCardException.class, ()->model.returnCreditCardPayment(1, "111111111111111"));
+    }
+
+    @Test
+    public void badReceiveCashPaymentTest() throws InvalidPasswordException, InvalidUsernameException, UnauthorizedException,InvalidProductCodeException, InvalidQuantityException, InvalidTransactionIdException, InvalidPaymentException {
+        model.login(admin_username, admin_psw);
+
+        Integer id = model.startSaleTransaction();
+        model.addProductToSale(id, productCode, 10);
+        model.endSaleTransaction(id);
+
+        assertEquals("If transaction does not exist, returned value should be -1" ,-1, model.receiveCashPayment(23, 100), 0.01);
+        assertEquals("If amount passed is not enough to pay returned value should be -1", -1, model.receiveCashPayment(id, 10), 0.01);
+    }
+
+    @Test
+    public void goodReceiveCashPayment() throws UnauthorizedException, InvalidProductCodeException, InvalidPasswordException, InvalidUsernameException, InvalidQuantityException, InvalidTransactionIdException, InvalidPaymentException {
+        model.login(admin_username, admin_psw);
+
+        Integer id = model.startSaleTransaction();
+        model.addProductToSale(id, productCode, 10);
+        model.endSaleTransaction(id);
+
+        assertEquals(50.,model.receiveCashPayment(id, 150), 0.01);
+    }
+
+    @Test
+    public void badReceiveCreditCardPayment() throws InvalidPasswordException, InvalidUsernameException, UnauthorizedException, InvalidQuantityException, InvalidTransactionIdException, InvalidProductCodeException, InvalidCreditCardException, IOException {
+        model.login(admin_username, admin_psw);
+
+        Integer id = model.startSaleTransaction();
+        model.addProductToSale(id, productCode, 10);
+        model.endSaleTransaction(id);
+        assertEquals(50, ez.getBalance().getSaleTransactionById(id).computeCost(), 0.01);
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("PaymentGateway/cards.txt"));
+        writer.write("#Comment\n#Comment\n4485370086510891;30");
+        writer.close();
+
+        assertFalse("sale not exists", model.receiveCreditCardPayment(23, "4485370086510891"));
+        assertFalse("not enough money",model.receiveCreditCardPayment(id, "4485370086510891"));
+
+        writer = new BufferedWriter(new FileWriter("PaymentGateway/cards.txt"));
+        writer.write("");
+        writer.close();
+        assertFalse("credit card is not present", model.receiveCreditCardPayment(id, "4485370086510891"));
+
+    }
+
+
 }
--- src/test/java/it/polito/ezshop/internalTests/IntegrationTest/BalanceTest.java
+++ src/test/java/it/polito/ezshop/internalTests/IntegrationTest/BalanceTest.java
@@ -12,17 +12,19 @@ import java.util.HashMap;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class BalanceTest {
-/*    BalanceModel b = new BalanceModel();
+    BalanceModel b = new BalanceModel();
     BalanceOperationModel credit;
     BalanceOperationModel debit;
-    SaleTransactionModel sale;*/
+
     String pC1 = "0123456789";
     String pC2 = "0123456189";
     OrderModel om1;
     OrderModel om2;
-    ArrayList<TicketEntryModel> productList = new ArrayList<>();
+    ArrayList<TicketEntryModel> productList1 = new ArrayList<>();
+    ArrayList<TicketEntryModel> productList2 = new ArrayList<>();
     SaleModel sm1 = new SaleModel();
     SaleModel sm2 = new SaleModel();
     HashMap<String, ProductTypeModel> map = new HashMap<>();
@@ -34,29 +36,16 @@ public class BalanceTest {
         om1.setStatus("ISSUED");
         om2 = new OrderModel(pC2, 500, 2.);
         om2.setStatus("ISSUED");
-        productList.add(new TicketEntryModel(pC1, "desc", 10, 5.));
-        productList.add(new TicketEntryModel(pC2, "desc", 10, 9.));
-        sm1.setProductList(productList);
-        sm2.setProductList(productList);
+        productList1.add(new TicketEntryModel(pC1, "desc", 10, 5.));
+        productList1.add(new TicketEntryModel(pC2, "desc", 10, 9.));
+        productList2.add(new TicketEntryModel(pC1, "desc", 10, 5.));
+        productList2.add(new TicketEntryModel(pC2, "desc", 10, 9.));
+        sm1.setProductList(productList1);
+        sm2.setProductList(productList2);
         map.put(pC1, new ProductTypeModel(1,"desc",pC1, 5., "note"));
         map.put(pC2, new ProductTypeModel(2,"desc",pC2, 9., "note"));
     }
 
-/*
-    @Test
-    public void getTransactionByIdTest(){
-        Optional<BalanceOperationModel> result = b.getTransactionById(credit.getBalanceId());
-        Assert.assertEquals(credit, result.orElse(null));
-        result = b.getTransactionById(debit.getBalanceId());
-        Assert.assertEquals(debit, result.orElse(null));
-        result = b.getTransactionById(sale.getBalanceId());
-        Assert.assertEquals(sale, result.orElse(null));
-        result = b.getTransactionById(100);
-        Assertions.assertNull(result.orElse(null));
-        result = b.getTransactionById(-10);
-        Assertions.assertNull(result.orElse(null));
-    }
-*/
 
     @Test
     public void test(){
@@ -99,8 +88,21 @@ public class BalanceTest {
         ReturnTransactionModel rtm2 = new ReturnTransactionModel(rm2);
         b.addReturnTransactionModel(rtm2.getBalanceId(), rtm2);
         rtm2.setPayment(new CashPaymentModel(rtm2.getMoney(), true, rtm2.getMoney()));
-        assertEquals(-1100+280-25-27-25, b.computeBalance(), 0.01);
+        assertEquals(-1100+280-25-27, b.computeBalance(), 0.01);
 
+        credit = new BalanceOperationModel("CREDIT", 50., LocalDate.now());
+        debit = new BalanceOperationModel("DEBIT", -50., LocalDate.now());
+        b.addBalanceOperation(credit);
+        b.addBalanceOperation(debit);
+        assertEquals(8 ,b.getAllBalanceOperations().size());
+        assertEquals(credit, b.getTransactionById(credit.getBalanceId()).orElse(null));
+        assertEquals(debit, b.getTransactionById(debit.getBalanceId()).orElse(null));
+
+        assertEquals(b.getAllBalanceOperations(), b.getCreditsAndDebits(null , null));
+        assertEquals(0, b.getCreditsAndDebits(LocalDate.now().plusDays(1), null).size());
+        assertEquals(b.getAllBalanceOperations(), b.getCreditsAndDebits(null, LocalDate.now().plusDays(1)));
+        assertEquals(b.getAllBalanceOperations(), b.getCreditsAndDebits(LocalDate.now().minusDays(1), LocalDate.now().plusDays(1)));
+        assertEquals(b.getAllBalanceOperations(), b.getCreditsAndDebits(LocalDate.now().plusDays(1), LocalDate.now().minusDays(1)));
     }
 
 
